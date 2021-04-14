16a21d991bcf56e7f4c6a14a7b6cfeab3f1f1a1b
wuyanjunxi <1335023818@qq.com>
2021-04-14 08:36:01 +0800

    在idea中提交一下试一试

--- 尝试创建第一个project(相当于eclipse中的workspace,里面可以创建modul(相当于eclipse中的project))/src(由于已经在该project下边创建了Module，所以也不需要在这个src下编写代码了，所以这个src可以手动删除掉了)/com/Test.java
+++ 尝试创建第一个project(相当于eclipse中的workspace,里面可以创建modul(相当于eclipse中的project))/src(由于已经在该project下边创建了Module，所以也不需要在这个src下编写代码了，所以这个src可以手动删除掉了)/com/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args){
         System.out.println("hello wordasdf");
         Object o = new Object();
+        //asdfasfasdfasdf
     }
 
     public  static void m(){
